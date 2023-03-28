d0cecc0a90d0a004b801155192bd2d8ef2b50d03
Tochi4R <tochukwu_samuel@yahoo.com>
2023-03-28 06:12:41 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- function1.c
+++ function1.c
@@ -1,12 +1,74 @@
-#include <stdio.h>
+/*
+ * print_int - Prints an integer to stdout
+ *
+ * print_int:
+ *
+ * @n the integer to print
+ *
+ * return: void
+ */
+void print_int(int n)
+{
+	printf("%d", n);
+}
+
 /**
- * main - A function that handle d and i
+ * _printf - Prints formatted output to stdout according to a format string.
+ *
+ * @format: the format string
+ *
+ * return: count
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'd':
+				case 'i':
+					{
+						int n = va_arg(args, int);
+
+						print_int(n);
+						count++;
+						break;
+					}
+				default:
+					putchar(*format);
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+
+			format++;
+		}
+		va_end(args);
+		return (count);
+}
+
+/**
+ * main -  entry point for the program
+ *
+ * return: the exit status of the program
  */
 int main(void)
 {
 	int n = 42;
 
-	printf("%d\n", n);
-	printf("%i\n", n);
+	_printf("The answer is %d\n", n);
 	return (0);
 }
