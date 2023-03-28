65e8ff64c8ad9fe2691749d5a7c7bcb946c506ac
Tochi4R <tochukwu_samuel@yahoo.com>
2023-03-28 13:36:55 -0700

    I'm not going anywhere

--- functions.c
+++ functions.c
@@ -1,124 +1,47 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdarg.h>
 #include <stdio.h>
 /**
- * print_char - writes the character c to stdout
- * @arg: argument
+ * _printf - a function that produces output
+ * according to a format
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @format: is a character string
+ *
+ * return: count
  */
-int print_char(va_list arg)
+int _printf(const char *format, ...)
 {
-	return (_putchar(va_arg(arg, int)));
-}
+	va_list args;
+	int count = 0;
+	char c;
 
-/**
- * print_int - prints an integer.
- * @arg: argument
- * Return: 0
- */
-
-int print_int(va_list arg)
-{
-
-unsigned int divisor = 1, i, resp, charPrinted = 0;
-int n = va_arg(arg, int);
-
-if (n < 0)
-{
-	_putchar('-');
-	charPrinted++;
-	n *= -1;
-}
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-return (charPrinted);
-}
-
-
-
-/**
- * print_STR - prints a string with a `S` (upper case) specificer
- * @arg: argument
- * Return: number of character printed
- */
-
-int print_STR(va_list arg)
-{
-int i;
-char *str = va_arg(arg, char*);
-
-if (str == NULL)
-	str = "(null)";
-else if (*str == '\0')
-	return (-1);
-
-for (i = 0; str[i]; i++)
-{
-	if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
+	va_start(args, format);
+	while ((c = *(format++)))
 	{
-		_putchar('\\');
-		_putchar('x');
-		if (i < 16)
-			_putchar('0');
-
-        	print_unsignedIntToHex(str[i], 'A');
+		if (c == '%')
+		{
+			c = *(format++);
+			switch (c)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += printf("%s", va_arg(args, char *));
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(c);
+			count++;
+		}
 	}
-	else
-		_putchar(str[i]);
-}
-
-return (i);
-}
-
-/**
- * print_str - prints a string with a `s` (lower case) specifier
- * @arg: argument
- * Return: number of character printed
- */
-
-int print_str(va_list arg)
-{
-int i;
-char *str = va_arg(arg, char*);
-
-if (str == NULL)
-	str = "(null)";
-else if (*str == '\0')
-	return (-1);
-
-for (i = 0; str[i]; i++)
-	_putchar(str[i]);
-
-return (i);
-}
-
-/**
- * print_unsigned - prints an unsigned int.
- * @arg: argument
- * Return: 0
- */
-
-int print_unsigned(va_list arg)
-{
-int divisor = 1, i, resp;
-unsigned int n = va_arg(arg, unsigned int);
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-return (i + 1);
+	va_end(args);
+	return (count);
 }
